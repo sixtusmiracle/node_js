75a88a62fd2751979cab6a9c2ab6bab669928119
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-03-30 16:49:30 +0100

    Profile App - Getting the response body

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
-console.log("Hello World");
-console.error("Oooops, something went wrong");
-console.dir({
-  name: "Sixtus",
-  age: 18
+/**
+ * A Command Line Application
+ * @name Profile_App
+ * @author Sixtus Miracle
+ */
+
+//! I need a simple way to look at a treehouse user's badge count and JS points
+//* Use Node.js to connect to Treehouse's API to get profile information and print out
+
+const https = require('https'); // require https module
+const username = "chalkers";
+
+/**
+ * prints message to the console
+ * 
+ * @param {string} username 
+ * @param {int} badgeCount 
+ * @param {int} points 
+ */
+function printMessage(username, badgeCount, points) {
+  const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+  console.log(message);
+}
+
+// Connect to the API URL (https://teamtreehouse.com/username.json)
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+  let body = "";
+
+  // Read the data
+  response.on('data', data => {
+    body += data.toString();
+  });
+
+  response.on('end', () => {
+    console.log(body);
+    console.log(typeof body);
+  });
+
+  // Parse the data
+  // Print the data
 });
