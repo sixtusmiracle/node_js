b49796a2501b33a715805be88aef206e844af475
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-05-30 09:24:08 +0100

    created simple web server

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-/**
- * A Command Line Application
- * @name Profile_App
- * @author Sixtus Miracle
- */
+//1. Create a web server
+const http = require('node:http');
 
-//! I need a simple way to look at a treehouse user's badge count and JS points
-//* Use Node.js to connect to Treehouse's API to get profile information and print out
+const hostname = '127.0.0.1';
+const port = 3000;
 
-const profile = require('./profile'); // .js is optional but the path ./ is mandatory
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  setInterval(() => response.write(new Date() + "\n"), 1000);
+  // response.end('Hello, World!\n');
+});
 
-// console.dir(process.argv.slice(2));
-// const users = ["chalkers", "alenaholligan", "davemcfarland"];
-const users = process.argv.slice(2);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
-// users.forEach(username => getProfile(username));
-users.forEach(profile.get); // shortened
+//2. handle HTTP route GET / and POST / i.e Home
+// if url == "/" && GET
+// show search
+// if url == "/" && POST
+// redirect to /:username
+
+//3. Handle HTTP route GET /:username i.e /chalkers
+// if url == "/..."
+// get json from treehouse api
+// on "end"
+// show profile
+// on "error"
+// show error
+
+//4. Function that handles the reading of files and merge in value
+// read from file and get a string
+// merge values into string
